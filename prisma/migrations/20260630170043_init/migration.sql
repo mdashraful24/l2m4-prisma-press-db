d914229fd31c093c5ae75b3e7ca41dec94eb4177
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-06-30 23:02:24 +0600

    added ondelete cascade

---
+++ prisma/migrations/20260630170043_init/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "logins" DROP CONSTRAINT "logins_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "profiles" DROP CONSTRAINT "profiles_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "logins" ADD CONSTRAINT "logins_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
